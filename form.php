4189198dc14767cf52ec4f924914db4cd9e6625f
Lhoover <lh0987936@otc.edu>
2025-09-12 10:43:21 -0500

    Initial Commit

---
+++ form.php
@@ -0,0 +1,4 @@
+<?php
+//Create a form with two inputs:
+    //Return Date (the date the book is or will be returned)
+    //Due Date (the date the book is due)
---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+//If Return Date is after Due Date:
+
+//Display how many years, months, and days the book is overdue.
+
+//If Return Date is before Due Date:
+
+//Display how many years, months, and days remain until the book is due.
+
+//If the dates are the same:
+
+//Display that the book is due today.
+    
+//Display the dates the user entered. 
+?>
